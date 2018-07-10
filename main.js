02059e526795904e583b7e39ec166e0306958944
chenxinda <chenxinda520@qq.com>
2018-07-11 00:52:45 +0800

    jQuery轮播作品

---
+++ main.js
@@ -0,0 +1,30 @@
+$('#btn1').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(0)'
+    })
+})
+$('#btn2').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(-800px)'
+    })
+})
+$('#btn3').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(-1600px)'
+    })
+})
+$('#btn4').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(-2400px)'
+    })
+})
+$('#btn5').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(-3200px)'
+    })
+})
+$('#btn6').on('click',function(){
+    $('.images').css({
+        transform: 'translateX(-4000px)'
+    })
+})
