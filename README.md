e151c2b41f6d2d1bc4ff79e51f97e9fdcbbf9597
chenxinda <chenxinda520@qq.com>
2018-07-17 11:36:37 +0800

    修改点击btn造成播放顺序错乱的BUG

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# carousel-demo
+# slieds-demo
 预览：https://simba-chen.github.io/carousel-demo/index.html
 
 这是一个jQuery实现轮播的小demo，实现自动播放功能
--- main.js
+++ main.js
@@ -1,60 +1,27 @@
-/*$('#btn1').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(0)'
-    })
-})
-$('#btn2').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(-800px)'
-    })
-})
-$('#btn3').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(-1600px)'
-    })
-})
-$('#btn4').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(-2400px)'
-    })
-})
-$('#btn5').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(-3200px)'
-    })
-})
-$('#btn6').on('click',function(){
-    $('.images').css({
-        transform: 'translateX(-4000px)'
-    })
-})*/
-//以后就可以不改动JS 增加轮播图片
-var allButtons = $('.btn>button')
-
-for(let i=0;i<allButtons.length;i++){
-    $(allButtons[i]).on('click', function(message){
-        var index = $(message.currentTarget).index() //jQuery中index()方法得到自己在父元素处于第几个子元素，并将数值返回出来
-        var n = index * -800
-        $('.images').css({
-            transform:'translate('+ n +'px)'
-        })
-        //解决鼠标选中button的bug
-        allButtons.eq(index)
-        .addClass('red')
-        .siblings('.red').removeClass('red')
-    })
-}
-
-//增加自动播放功能
-var x = 0
+var $allButtons = $('.btn>button')
+var index
 var num = $('.images>img').length
+var x = 0
 
+$allButtons.on('click', function(message){
+    index = $(message.currentTarget).index() //jQuery中index()方法得到自己在父元素处于第几个子元素，并将数值返回出来
+    var n = index * -800
+    x = index
+    $('.images').css({
+        transform:'translate('+ n +'px)'
+    })
+    //解决鼠标选中button的bug
+    $allButtons.eq(index)
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+    console.log(index)
+})
+//增加自动播放功能
 var timerId = setInterval(()=>{
     x += 1
-    allButtons.eq(x%num).trigger('click')
-        .addClass('red') //选中第几个子元素并以jQuery对象返回出来，帮助触发事件
+    $allButtons.eq(x%num).trigger('click') //.eq()选中第几个子元素并以jQuery对象返回出来，帮助触发事件
+        .addClass('red')
         .siblings('.red').removeClass('red')
-//jQuery链式操作，每次找到元素后，后面的操作就把该元素当作操作对象。
 },1500)
 $('.window').on('mouseenter', function(){
     window.clearInterval(timerId)
@@ -62,7 +29,7 @@ $('.window').on('mouseenter', function(){
 $('.window').on('mouseleave', function(){
     timerId = setInterval(()=>{
         x += 1
-        allButtons.eq(x%num).trigger('click')
+        $allButtons.eq(x%num).trigger('click')
             .addClass('red') 
             .siblings('.red').removeClass('red')
     },1500)
